8769264cc8f7f83ab373b5f8232bd642e2c178a5
FabianaFerreira <fabiana.ferreira@poli.ufrj.br>
2018-08-31 19:01:30 -0300

    Tratando entrada de comandos não existentes

--- Trabalho01/Trabalho1_FabianaFerreiraFonseca.c
+++ Trabalho01/Trabalho1_FabianaFerreiraFonseca.c
@@ -98,11 +98,37 @@ void freeArray(char** arguments)
 int main () 
 {	
 	FILE* stream;
+	char commandList[BUFFER*20];
 	char userInput [BUFFER];
+	char inputCopy [BUFFER];
 	/*Alocando memoria para criar o array que armazenará os argumentos*/
 	char** arguments = (char**)malloc(BUFFER*sizeof(char*));
 	char* pathOutput = (char*)malloc(sizeof(char*));
 	unsigned exit = 0;
+	int pipefd[2];
+
+	/*Faço uma chamada de execv para listar os comandos que estão na 
+	pasta bin, de forma a conseguir tratar o erro, caso o usuário entre
+	com um comando que não está na pasta*/
+	pipe(pipefd);
+	pid_t binDir_pid = fork();
+	if (binDir_pid == 0)
+	{
+		//Fecha o lado de entrada do pipe (do filho)
+		close(pipefd[0]);
+
+		dup2(pipefd[1], fileno(stdout));
+		close(pipefd[1]);
+
+		execlp("/bin/dir", "dir","-1","/bin/",NULL);
+	}
+	else 
+	{
+		wait(NULL);
+		close(pipefd[1]);
+		while(read(pipefd[0], commandList, sizeof(commandList)) != 0) {}
+	} 
+	
 	printf(CYAN_COLOR);
 	printf("\n/******************************************************************************/\n");
 	printf("/*------ FABITS SHELL: UM SHELL SIMPLIFICADO PARA SISTEMAS OPERACIONAIS ------*/\n");
@@ -120,8 +146,29 @@ int main ()
 		{
 			/*Tratando a retirada do caracter de retorno que vem com a chamada da fgets*/
 			unsigned inputLength = strlen(userInput);
-			if (inputLength > 0 && userInput[inputLength - 1] == '\n')
-				userInput[--inputLength] = '\0';
+			
+			/*String auxiliar para pegar apenas o nome do comando e nao as suas opcoes
+			na hora de checar sua existencia*/
+			strcpy(inputCopy,userInput);
+			strtok(inputCopy, DELIMITER);
+	
+			/*Tratando o caso em que o usuário apenas dá enter e quando o comando não existe*/
+			if ((inputLength == 1 && userInput[inputLength - 1] == '\n') || inputLength == 2) 
+			{	
+				printf("Digite um comando válido\n");
+				continue;
+			}
+			if (strstr(commandList, inputCopy) == NULL && strstr(inputCopy,EXIT_COMMAND) == NULL) 
+			{				
+				printf("Comando não existente. Por favor, digite novamente.\n");
+				continue;
+			}
+
+			/*------Digitou um comando que existe na pasta /bin-----*/
+
+			/*Se nao caiu em nenhum dos casos anteriores, pega o input e tira o \n*/		
+			if (inputLength > 1 && userInput[inputLength - 1] == '\n') 
+				userInput[inputLength - 1] = '\0';					
 			
 			/*Entrou com um comando que não é o exit*/
 			if (strcmp(userInput,EXIT_COMMAND) != 0) 
@@ -156,7 +203,7 @@ int main ()
 				/*PARENT*/
 					wait(NULL);						
 				}					
-			free(commandPath);
+			free(commandPath);			
 			}
 			else 
 			{
@@ -169,6 +216,5 @@ int main ()
 	}
 	/*Liberado o espaço de memoria alocado pelo proprio array*/	
 	free(arguments);
-	
 	return 0;
 }
