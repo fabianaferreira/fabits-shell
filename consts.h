95e8df0c0d2aa497b1c39d58a48b24d81a5bb129
Fabiana Ferreira Fonseca <fabiana.ferreira@poli.ufrj.br>
2018-12-05 18:25:06 -0200

    Removendo arquivos nao utilizados

---
+++ consts.h
@@ -0,0 +1,25 @@
+/*Universidade Federal do Rio de Janeiro
+  Departamento de Engenharia Eletrôncia e de Computação
+  Sistemas Operacionais - 2018.2
+  Aluna: Fabiana Ferreira Fonseca
+  DRE: 115037241*/
+
+/*TRABALHO: IMPLEMENTAÇÃO DE UM SHELL PARA LINUX COM MULTIPLAS TELAS*/
+
+#define EOS			  	      "\0"
+#define BUFFER			      100
+#define DELIMITER 		    " "
+#define CLEAR_COMMAND 	  "clear\n"
+#define SCREEN_COMMAND 	  "screen"
+#define EXIT_COMMAND 	    "exit\n"
+#define MAN_COMMAND		    "man fabits\n"
+#define MONITOR_COMMAND		"monitor\n"
+#define PATH 			        "/bin/"
+#define CYAN_COLOR		    "\033[1;36m"
+#define GREEN_COLOR	  	  "\033[1;32m"
+#define RED_COLOR		      "\033[1;31m"
+#define MAGENTA_COLOR	    "\033[1;35m"
+#define RESET_COLOR 	    "\033[0m"
+#define FIFO_PATH		      "./.files/"
+#define STATUS_CPU_PATH   "/proc/stat"
+#define STATUS_RAM_PATH   "/proc/self/status"
